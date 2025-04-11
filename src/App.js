46ff568ce5a2676d7daae0888c0f018b16d25481
@umanijaz <umanijaz5@gmail.com>
2025-04-12 00:48:15 +0500

    basic React Router setup|

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
 function App() {
   return (
     <div className="App">
       <h1>Hola, Amigo!</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
